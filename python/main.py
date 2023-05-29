f9bb04a998aac588818f53bc2ba8ec9137e72a12
Mário <123200208+mariocerutti@users.noreply.github.com>
2023-05-29 09:24:50 -0300

    First file of python
    File

---
+++ python/main.py
@@ -0,0 +1,8 @@
+print("Hello, wordl")
+n = 0
+acomulador = 0
+tamanho = int(input("Digite um número: "))
+for c in range (tamanho):
+    acomulador += 1
+    print(acomulador)
+print("Fim do código")
